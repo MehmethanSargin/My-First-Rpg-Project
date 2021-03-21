29018abcde96e502d0b55c58ab55f995f5f9bd8b
Mehmethan Sargin <mehmethansargin123@gmail.com>
2021-03-22 02:48:05 +0300

    style(name): Player movement change name to mover. #.

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class PlayerMovement : MonoBehaviour
+public class Mover : MonoBehaviour
 {
     void Update()
     {
